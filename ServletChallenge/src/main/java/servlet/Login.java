132e024335ac516c2b7868ea8c78fff3cb8de537
Basilio Lopez III <batcho904@gmail.com>
2018-10-12 11:36:46 -0500

    Second Commit

--- ServletChallenge/src/main/java/servlet/Login.java
+++ ServletChallenge/src/main/java/servlet/Login.java
@@ -2,17 +2,17 @@ package servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/Login")
+//@WebServlet("/Login")
+@SuppressWarnings("serial")
 public class Login extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-
+	
+	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		PrintWriter pw = resp.getWriter();
@@ -20,7 +20,8 @@ public class Login extends HttpServlet {
 		pw.println("Login");
 		pw.close();
 	}
-
+	
+	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) 
 			throws ServletException, IOException {
 		PrintWriter pw = resp.getWriter();
