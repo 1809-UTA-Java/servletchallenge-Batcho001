c5007ffdb77788d4f4656ee74fb2b3c3accefd44
Basilio Lopez III <batcho904@gmail.com>
2018-10-12 11:08:16 -0500

    First Commit
    Test Commit

---
+++ ServletChallenge/src/main/java/servlet/Login.java
@@ -0,0 +1,30 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		PrintWriter pw = resp.getWriter();
+		
+		pw.println("Login");
+		pw.close();
+	}
+
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) 
+			throws ServletException, IOException {
+		PrintWriter pw = resp.getWriter();
+		pw.println("POST");
+		pw.close();
+	}
+}
